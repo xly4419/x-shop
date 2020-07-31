8dcb9137fe4468880c9d6fedc0313bd7ae2394c3
xly <xly0912@gmail.com>
2020-07-31 17:18:02 +0800

    构建项目、配置全局样式、路由

---
+++ src/views/Login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="login">
+    这是登录页
+    <el-button>默认按钮</el-button>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
