59c12e444a3a83a507b732bb271451e65c1ded27
xly <xly0912@gmail.com>
2020-08-22 18:47:28 +0800

    更改

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>用户管理</div>
+  <div>
+    <div>
+      <el-row>
+        <el-col :span="18"></el-col>
+        <el-col :span="6">
+          <el-button type="error">新增</el-button>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
 </template>
 <script>
 export default {};
